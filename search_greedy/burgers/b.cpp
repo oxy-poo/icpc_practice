9261d785b57da08d480e27e6229e32ccffe43ba2
Akshay Gulabrao <agula002@wch135-06.cs.ucr.edu>
2020-01-11 15:09:29 -0800

    working b

--- search_greedy/burgers/b.cpp
+++ search_greedy/burgers/b.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
-	ifstream inFS;
-	inFS.open("i.dat");
+	int q;
+	cin >> q;
+	while(q-- > 0){
+		char b,p,f,h,c;
+		cin >> b >> p >> f >> h >> c;
+		b/=2;
+		int choice = max(h,c) * b;
+	}
 	
 	
 	return 0;
